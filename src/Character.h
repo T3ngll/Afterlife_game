460c1fd4127eb2ea40ce4ff01b85e1327dd8b55f
T3ngll <max.korolevsky.s@gmail.com>
2023-03-14 17:07:25 +0100

    make it more object oriented

--- src/Character.h
+++ src/Character.h
@@ -1,18 +1,52 @@
 #pragma once
 
 
-struct Character
+class Character
 {
-    float x=GetScreenWidth() / 2 - 25;
-    float y=GetScreenHeight() / 2 - 35;
-    float height=70;
-    float width=50;
-    float speedX=300;
-    float speedY=300;
-    float health=100;
-    float ammo=20;
+    float x;
+    float y;
+    float height;
+    float width;
+    float speedX;
+    float speedY;
+    float health;
+    float ammo;
+    float rotation;
+    Vector2 mousePosition = {0};
+public:
+    Character(float height, float width, float speedX, float speedY, float health, float ammo):height(height),width(width),speedX(speedX),speedY(speedY),health(health),ammo(ammo)
+    {
+        x=GetScreenWidth() / 2 -width/2;
+        y=GetScreenHeight() / 2 -height/2;
+    };
+    float getX() const {return x;}
+    void setX(float xNew) {x+=xNew;}
+    float getY() const {return y;}
+    void setY(float yNew) {y+=yNew;}
+    float getHeight() const {return height;}
+    float getWidth() const {return width;}
+    float getSpeedX() const {return speedX;}
+    float getSpeedY() const {return speedY;}
+    float getHealth() const {return health;}
+    Vector2 getVector() {return mousePosition;}
+    float getRotation() const {return rotation;}
+
+    //void Draw(float x,float y, float width,float height);
+
 
 };
+/*
+void Character::Draw(float x,float y, float width,float height)
+{
+    //Rectangle rec={character.x,character.y,character.width, character.height};
+    DrawRectangle(x,y,width,height,LIME);
+    //DrawRectanglePro(rec,(Vector2){character.x,character.y},rotation * RAD2DEG,BLACK);
+
+}*/
+
+
+
+
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 
 
-int main(void)
+int main()
 {
 
     InitWindow(800,800, "Afterlife");
@@ -21,7 +21,7 @@ int main(void)
 
     UnloadImage(background);
 
-    Character player;
+    Character player(70,50,300,300,100,20);
 
 
     //cout << "Run path: " << GetWorkingDirectory() << endl;
@@ -29,21 +29,21 @@ int main(void)
     {
 
 
-        if (IsKeyDown(KEY_D) && player.x < (GetScreenWidth()-player.width)) player.x+= player.speedX*GetFrameTime();
-        if (IsKeyDown(KEY_A) && player.x > 1) player.x -= player.speedX*GetFrameTime();
-        if (IsKeyDown(KEY_W) && player.y >0) player.y -= player.speedY*GetFrameTime();
-        if (IsKeyDown(KEY_S) && player.y < (GetScreenHeight()-player.height)) player.y  += player.speedY*GetFrameTime();
+        if (IsKeyDown(KEY_D) && player.getX() < (GetScreenWidth()-player.getWidth())) player.setX(player.getSpeedX()*GetFrameTime()) ;
+        if (IsKeyDown(KEY_A) && player.getX() > 1) player.setX(-1*player.getSpeedX()*GetFrameTime());
+        if (IsKeyDown(KEY_W) && player.getY() >0) player.setY(-1*player.getSpeedY()*GetFrameTime()) ;
+        if (IsKeyDown(KEY_S) && player.getY() < (GetScreenHeight()-player.getHeight())) player.setY(player.getSpeedY()*GetFrameTime());
 
 
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawTexture(texture, 0, 0, RAYWHITE);
+        DrawTexture(texture, 0, 0, WHITE);
 
         //DrawText("placeholder", 10, 10, 20, DARKGRAY);
-
-    DrawRectangle(player.x, player.y, player.width, player.height, BLACK);
+        DrawRectangle(player.getX(),player.getY(),player.getWidth(),player.getHeight(),BLACK);
+       // player.Draw(player,player.getVector(),player.getRotation(),BLACK);
 
 
         DrawFPS(10,10);
