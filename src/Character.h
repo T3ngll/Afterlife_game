e47cd7acea448d51d0f4a8ba3b82a0bb2bad23aa
T3ngll <max.korolevsky.s@gmail.com>
2023-03-14 01:04:32 +0100

    Speed fix

--- src/Character.h
+++ src/Character.h
@@ -1,6 +1,20 @@
 #pragma once
 
 
+struct Character
+{
+    float x=GetScreenWidth() / 2 - 25;
+    float y=GetScreenHeight() / 2 - 35;
+    float height=70;
+    float width=50;
+    float speedX=200;
+    float speedY=200;
+    float health=100;
+    float ammo=20;
+
+};
+
+
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,23 @@
 #include "raylib.h"
 #include "Character.h"
 
-struct Character
-{
-    float x,y;
-    float height,width;
-    float speedX,speedY;
-    float health;
-    float ammo;
 
-};
+
+
 
 int main(void)
 {
 
     InitWindow(800,800, "Afterlife");
-    SetTargetFPS(60);
+
+
+    //SetTargetFPS(100);
     SetWindowState(FLAG_VSYNC_HINT);
 
-
+    Texture2D Background = LoadTexture("./resources/hall.png");
 
     Character player;
-    player.x= GetScreenWidth() / 2 - 25;
-    player.y= GetScreenHeight() / 2 - 35;
-    player.speedX=50;
-    player.speedY=50;
-    player.height=70;
-    player.width=50;
+
 
 
 
@@ -34,25 +25,27 @@ int main(void)
     {
 
 
-        if (IsKeyDown(KEY_RIGHT) && player.x < (GetScreenWidth()-player.width)) player.x+= player.speedX;
-        if (IsKeyDown(KEY_LEFT) && player.x > 1) player.x -= player.speedX;
-        if (IsKeyDown(KEY_UP) && player.y >0) player.y -= player.speedY;
-        if (IsKeyDown(KEY_DOWN) && player.y < (GetScreenHeight()-player.height)) player.y  += player.speedY;
+        if (IsKeyDown(KEY_RIGHT) && player.x < (GetScreenWidth()-player.width)) player.x+= player.speedX*GetFrameTime();
+        if (IsKeyDown(KEY_LEFT) && player.x > 1) player.x -= player.speedX*GetFrameTime();
+        if (IsKeyDown(KEY_UP) && player.y >0) player.y -= player.speedY*GetFrameTime();
+        if (IsKeyDown(KEY_DOWN) && player.y < (GetScreenHeight()-player.height)) player.y  += player.speedY*GetFrameTime();
 
 
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
+        DrawTexture(Background,0,0,WHITE);
 
-            DrawText("", 10, 10, 20, DARKGRAY);
+        //DrawText("placeholder", 10, 10, 20, DARKGRAY);
 
-        DrawRectangle(player.x, player.y, player.width, player.height, BLACK);
+    DrawRectangle(player.x, player.y, player.width, player.height, BLACK);
 
 
-            DrawFPS(10,10);
+        DrawFPS(10,10);
 
         EndDrawing();
     }
+    UnloadTexture(Background);
 
     CloseWindow();
 
